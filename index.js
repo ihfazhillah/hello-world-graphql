e1e8abfa8ff02b57f9baee70f92694c7b7725258
ihfazhillah <mihfazhillah@gmail.com>
2018-01-23 10:50:35 +0700

    graphql endpoint

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var express = require("express"),
+  makeExecutableSchema = require("graphql-tools").makeExecutableSchema,
+  graphqlExpress = require("apollo-server-express").graphqlExpress,
   app = express();
 
-app.use("/graphql", (req, res, next) => {
-  res.send("hello world, endpoint ini akan kita jadikan graphql endpoint");
-});
+var typeDefs = `
+type Query{
+  hello: String
+}`;
+
+var resolvers = {
+  Query: {
+    hello() {
+      return "hello world";
+    }
+  }
+};
+
+var schema = makeExecutableSchema({ typeDefs: typeDefs, resolvers: resolvers });
+
+app.use("/graphql", graphqlExpress({ schema: schema }));
 
 app.listen(3000, () => console.log("server berjalan di http://localhost:3000"));
